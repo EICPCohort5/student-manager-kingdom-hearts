6eb195989974e662a666d955e0dfdf5b492889d7
Christopher Zarba <chr13545@tjx.com>
2022-02-24 11:30:37 -0500

    merge

--- js/main.js
+++ js/main.js
@@ -1,2 +1,35 @@
 // Your main JavaScript code goes here
 // This is set up as an ECMAScript module, so you can import other ES Modules
+
+const form = document.querySelector('#student-form');
+form.addEventListener('submit', handleFormSubmit);
+
+function handleFormSubmit(event) {
+    // DO NOT actually submit the form
+    event.preventDefault();
+    let studentData = new FormData(form)
+    
+}
+
+function iterateWithKeys(studentData) {
+    console.log('Iterating with keys:');
+    for (let key of studentData.keys()) {
+      console.log(`${key}: ${studentData.get(key)}`);
+    }
+  }
+class Student {
+
+    constructor ( firstName, lastName, location) {
+        this.firstname = firstName;
+        this.lastname = lastName;
+        this.location = location;
+    }
+
+    getName() {
+        return `${this.firstname} ${this.lastname}`;
+    }
+
+}
+
+
+
