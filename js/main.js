73fd9ac9e44598819fdc7e123d84b40fa5ed754b
Callum Ogle <cal51592@tjx.com>
2022-02-24 16:49:04 +0000

    added function to put student data from form to table

--- js/main.js
+++ js/main.js
@@ -4,32 +4,17 @@ const form = document.querySelector('#student-form');
 form.addEventListener('submit', handleFormSubmit);
 
 function handleFormSubmit(event) {
-    // DO NOT actually submit the form
-    event.preventDefault();
-    let studentData = new FormData(form)
-
+  // DO NOT actually submit the form
+  event.preventDefault();
+  let studentData = new FormData(form)
+  let table = document.querySelector('.table');
+  let row = document.createElement('tr')
+  console.log(studentData)
+  for (let key of studentData.keys()) {
+    let studentprop = document.createElement('td');
+    studentprop.textContent = studentData.get(key);
+    row.append(studentprop);
+    console.log(`${key}: ${studentData.get(key)}`);
+  }
+  table.append(row)
 }
-
-function iterateWithKeys(studentData) {
-    console.log('Iterating with keys:');
-    for (let key of studentData.keys()) {
-        console.log(`${key}: ${studentData.get(key)}`);
-    }
-}
-class Student {
-
-    constructor(firstName, lastName, location) {
-        this.firstname = firstName;
-        this.lastname = lastName;
-        this.location = location;
-    }
-
-    getName() {
-        return `${this.firstname} ${this.lastname}`;
-    }
-
-}
-
-
-
->>>>>>> 6eb195989974e662a666d955e0dfdf5b492889d7
