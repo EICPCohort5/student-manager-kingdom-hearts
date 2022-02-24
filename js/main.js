a86decc401b0de25188b3eaa064a8e460b0188aa
Callum Ogle <cal51592@tjx.com>
2022-02-24 16:35:27 +0000

    bleh

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 // Your main JavaScript code goes here
 // This is set up as an ECMAScript module, so you can import other ES Modules
+document.addEventListener('submit',addstudent)
+const form = document.getElementById('student-form')
+function addstudent(event){
+    event.preventDefault()
+    let studentdata = new FormData(form)
+    for (let i of studentdata){
+        console.log(i)
+    }
+}
